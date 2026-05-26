44fae5e314e6739a1ce992df412f1f24c9126ac2
Polabathina Ramcharan Teja <ramcharanpolabathina@gmail.com>
2026-05-27 00:17:48 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-2.java
@@ -0,0 +1,42 @@
+class Solution {
+    private int[][] memo;
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        memo = new int[n][2];
+
+        for(int[] row : memo){
+            Arrays.fill(row, - 1);
+        }
+
+        return dfs(prices, 0, 1);
+    }
+
+    private int dfs(int[] prices, int i, int canBuy){
+
+        if(i >= prices.length) return 0;
+
+        int profit = 0;
+
+        if(memo[i][canBuy] != -1){
+            return memo[i][canBuy];
+        }
+
+        if(canBuy == 1){
+            int skip = dfs(prices, i + 1, 1);
+
+            int buy = -prices[i] + dfs(prices, i + 1, 0);
+
+            profit = Math.max(skip, buy);
+        }else{
+            int skip = dfs(prices, i + 1, 0);
+
+            int sell = prices[i] + dfs(prices, i + 2, 1);
+
+            profit = Math.max(skip, sell);
+        }
+
+        return memo[i][canBuy] = profit;
+    }
+}
