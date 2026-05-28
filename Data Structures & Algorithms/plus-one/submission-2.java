29a0a11516d35c67569595ee44481fe914d76048
Polabathina Ramcharan Teja <ramcharanpolabathina@gmail.com>
2026-05-28 12:55:41 +0530

    Add: plus-one - submission-2

---
+++ Data Structures & Algorithms/plus-one/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        if (digits[n - 1] < 9) {
+            digits[n - 1]++;
+            return digits;
+        }
+        
+        for(int i = n - 1; i >= 0; i --){
+            if(digits[i] < 9){
+                digits[i] ++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] res = new int[n + 1];
+        res[0] = 1;
+        return res;
+    }
+}
